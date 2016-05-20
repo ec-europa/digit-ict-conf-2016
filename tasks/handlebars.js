d213f56fc2ef748ec03223bafdefe75816399f9c
Yannick Huard <huard.yan@gmail.com>
2016-05-20 16:57:16 +0200

    feat(example): small example using Handlebarjs

--- tasks/handlebars.js
+++ tasks/handlebars.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const hb = require('gulp-hb');
 
 gulp.task('handlebars', () => gulp
-  .src('./src/**/*.html')
+  .src('./src/*.html')
   .pipe(hb({
     partials: './src/assets/partials/**/*.hbs',
     data: './src/data/**/*.{js,json}',
