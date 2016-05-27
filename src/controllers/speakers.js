8bc9a900bc2ddddbbb2f9ab683405f1a32d09c68
Yannick Huard <huard.yan@gmail.com>
2016-05-27 11:48:27 +0200

    feat(router): cache speakers in state

--- src/controllers/speakers.js
+++ src/controllers/speakers.js
@@ -2,10 +2,25 @@ import view from '../views/speakers.js';
 import { fetchContent } from '../utils/fetchContent';
 
 export default (ctx, next) => {
+  // Set current page
   ctx.currentPage = 'speakers';
+
+  // Get DOM element to populate
   const main = window.document.querySelector('main');
-  fetchContent('data/speakers.json', (speakers) => {
-    main.innerHTML = view(speakers);
-  });
+
+  if (ctx.state.speakers) {
+    // If speakers are cached, display them
+    main.innerHTML = view(ctx.state.speakers);
+  } else {
+    fetchContent('data/speakers.json', (speakers) => {
+      // Cache speakers for further use
+      ctx.state.speakers = speakers;
+      ctx.save();
+
+      // Display speakers
+      main.innerHTML = view(speakers);
+    });
+  }
+
   next();
 };
