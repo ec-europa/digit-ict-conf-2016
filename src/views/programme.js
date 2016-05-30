3da31248f7bdd9e874f49c5192fc6e81cc02903c
Yannick Huard <huard.yan@gmail.com>
2016-05-30 21:12:51 +0200

    feat(design): better basic design

--- src/views/programme.js
+++ src/views/programme.js
@@ -1,1 +1,8 @@
-export default () => '<h1>Programme</h1>';
+export default () => `
+<div class="mdl-grid ict-max-width-900">
+  <div class="mdl-cell mdl-cell--12-col">
+    <h1>Programme</h1>
+    <p>Description</p>
+  </div>
+</div>
+`;
--- src/views/speakers.js
+++ src/views/speakers.js
@@ -1,9 +1,20 @@
 export default (speakers) => `
-  <h1>Speakers</h1>
-  <p>Short description</p>
-  <ul>
-  ${speakers.map(speaker => `
-      <li>${speaker.firstname}, ${speaker.lastname}</li>
-  `).join('')}
-  </ul>
+  <div class="mdl-grid ict-max-width-900">
+    <div class="mdl-cell mdl-cell--12-col">
+      <h1>Speakers</h1>
+      <p>Small description</p>
+    </div>
+  </div>
+  <div class="mdl-grid ict-max-width-900">
+    ${speakers.map(speaker => `
+        <div class="mdl-cell mdl-cell--4-col speaker_col">
+          <img class="speaker_thumbnail speaker_thumbnail--${['top','bottom','left','right'][Math.floor(Math.random()*4)]}"
+            src="/assets/images/speakers/${speaker.picture}"
+            alt="${speaker.firstname} ${speaker.lastname}" />
+          <div class="speaker_name">
+            ${speaker.firstname} ${speaker.lastname}
+          </div>
+        </div>
+    `).join('')}
+  </div>
 `;
