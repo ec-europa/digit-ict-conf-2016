52eeca2d71a0619fbd54153e41479ab67264426d
Yannick Huard <huard.yan@gmail.com>
2016-07-05 13:44:44 +0200

    feat(serve): prod server

---
+++ tools/scripts/prodServer.js
@@ -0,0 +1,21 @@
+/**
+ * Require Browsersync
+ */
+const browserSync = require('browser-sync');
+const history = require('connect-history-api-fallback');
+const path = require('path');
+
+/**
+ * Run Browsersync
+ */
+browserSync({
+  server: {
+    baseDir: path.resolve(process.cwd(), ''),
+    middleware: [
+      history({
+        index: '/build/index.html',
+      }),
+    ],
+  },
+  open: 'external',
+});
