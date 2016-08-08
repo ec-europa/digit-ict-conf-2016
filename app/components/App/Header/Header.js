8af5bbd4fcadc4e6ced1d7351f5f0e55081cb2f0
Yannick Huard <huard.yan@gmail.com>
2016-08-08 15:31:34 +0200

    feat(a11y): pin header when child node is focused

--- app/components/App/Header/Header.js
+++ app/components/App/Header/Header.js
@@ -10,6 +10,17 @@ import classnames from 'classnames';
 import styles from './Header.scss';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Bindings
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+
+    // Init
+    this.header = null;
+    this.headroom = null;
+  }
+
   componentDidMount() {
     this.headroom = new Headroom(this.header, {
       offset: 80,
@@ -21,6 +32,7 @@ class Header extends React.Component {
       },
     });
     this.headroom.init();
+    document.addEventListener('keydown', this.handleKeyDown);
   }
 
   shouldComponentUpdate(nextProps) {
@@ -28,7 +40,15 @@ class Header extends React.Component {
   }
 
   componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
     this.headroom.destroy();
+  }
+
+  handleKeyDown(event) {
+    // Press on TAB
+    if (event.keyCode === 9 && this.header.contains(event.target)) {
+      this.headroom.pin();
+    }
   }
 
   render() {
