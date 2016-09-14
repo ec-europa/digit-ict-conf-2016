449fab1cba72e204376de374faebcfaf58a1ccdf
Yannick Huard <huard.yan@gmail.com>
2016-09-14 15:36:12 +0200

    feat(design): better speakers cards

--- app/ui/components/Speakers/Speaker.js
+++ app/ui/components/Speakers/Speaker.js
@@ -22,10 +22,12 @@ class Speaker extends React.PureComponent {
         <div className={styles.pictureFrame}>
           <img className={styles.picture} src={speaker.picture} alt={`${speaker.firstname} ${speaker.lastname}`} />
         </div>
-        <div className={styles.name}>
-          {speaker.firstname} <span className={styles.lastname}>{speaker.lastname}</span>
+        <div className={styles.info}>
+          <div className={styles.name}>
+            {speaker.firstname} <span className={styles.lastname}>{speaker.lastname}</span>
+          </div>
+          <div className={styles.title}>{speaker.title}</div>
         </div>
-        <div className={styles.title}>{speaker.title}</div>
       </Link>
     );
   }
