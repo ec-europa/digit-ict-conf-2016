35d57f27cf95d32256bfcf9a1e81ecab06f825a5
Yannick Huard <mail@yannickhuard.com>
2017-05-03 10:19:27 +0200

    Make sure to pass NODE_ENV=production

--- tools/webpack/webpack.prod.babel.js
+++ tools/webpack/webpack.prod.babel.js
@@ -39,6 +39,12 @@ module.exports = require('./webpack.base.babel')({
   }),
 
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+
     new webpack.LoaderOptionsPlugin({
       options: {
         context: __dirname,
